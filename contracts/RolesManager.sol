6d65665ca3510ce0a2fa6323b2af39f7a0eeba02
Yuriy Chudinov <74604312+chudinovuvaws@users.noreply.github.com>
2020-11-20 18:30:52 +0200

    roles access manager

---
+++ contracts/RolesManager.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.5.16;
+
+
+contract RolesManager {
+    /// @notice Indicator that this is a PriceOracle contract (for inspection)
+    bool public constant isPriceOracle = true;
+
+   
+}
