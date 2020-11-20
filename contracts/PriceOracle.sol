38153240e6e0813bbe716708c84599bd705f62ec
Yuriy Chudinov <74604312+chudinovuvaws@users.noreply.github.com>
2020-11-20 17:49:24 +0200

    base files

---
+++ contracts/PriceOracle.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.5.16;
+
+import "./OCCToken.sol";
+
+contract PriceOracle {
+    /// @notice Indicator that this is a PriceOracle contract (for inspection)
+    bool public constant isPriceOracle = true;
+
+    /**
+      * @notice Get the underlying price of a Token asset
+      * @param Token The Token to get the underlying price of
+      * @return The underlying asset price mantissa (scaled by 1e18).
+      *  Zero means the price is unavailable.
+      */
+    function getUnderlyingPrice(OccToken occToken) external view returns (uint);
+}
