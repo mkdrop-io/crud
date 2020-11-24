1f0d6cebb954af55e65e8c2f1b67689c3c8b2eb4
Yuriy Chudinov <74604312+chudinovuvaws@users.noreply.github.com>
2020-11-25 00:43:00 +0200

    base logic (deposit, withdraw, info)

--- contracts/Pool.sol
+++ contracts/Pool.sol
@@ -1,59 +1,59 @@
+pragma solidity ^0.5.0;
 
-  pragma solidity ^0.4.23;
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+import "@openzeppelin/contracts/utils/EnumerableSet.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./OCCToken.sol";
 
-import "../../abstract/Pool/Pool.sol";
-import "../../abstract/TimeMachine/TimeMachineP.sol";
-
-contract PoolProd is Pool, TimeMachineP {
-  uint constant DECIMAL_MULTIPLIER = 1e18;
+contract Pool {
   
-  /**
-   * @param _raisingPeriod time to raise ETH for ICO
-   * @param _icoPeriod time to wait tokens from ICO manager
-   * @param _distributionPeriod time to distribute tokens and remaining ETH to investors
-   * @param _minimalFundSize minimal collected fund in ETH
-   * @param _maximalFundSize maximal collected fund in ETH
-   * @param _minimalDeposit minimal amount of ETH in wei which is allowed to become investor
-   * @param _adminShare share of the contract creator in percent, multiplied by 1e18, divided by 100
-   * @param _poolManagerShare share of the pool manager in percent, multiplied by 1e18, divided by 100
-   * @param _poolManagerAddress address of pool manager
-   * @param _ICOManagerAddress address of ICO manager
-   * @param  _paybotAddress address of pay bot
-   * @param _tokenPrice price of one erc20 token in ethers (if zero - ICOManager determine number of tokens by authority)
-   */
-  constructor(uint _raisingPeriod, 
-              uint _icoPeriod, 
-              uint _distributionPeriod, 
-              uint _minimalFundSize, 
-              uint _maximalFundSize, 
-              uint _minimalDeposit,
-              uint _adminShare, 
-              uint _poolManagerShare,
-              address _poolManagerAddress,
-              address _ICOManagerAddress,
-              address _paybotAddress,
-              address _tokenAddress,
-              uint _tokenPrice) public {
+
+  event Deposit(
+        address indexed _reserve,
+        address indexed _user,
+        uint256 _amount,
+        uint256 _timestamp,
+        uint256 sharesCreated
+    );
+
+  event Withdraw(
+        address indexed _reserve,
+        address indexed _user,
+        uint256 _amount,
+        uint256 _timestamp,
+        uint256 sharesRemoved
+    );
+
+    struct UserInfo {
+        uint256 amount; // How many investments the user has provided.
+        uint256 bonusToken; // Reward for investment (token)?
+   
+        //
+        // no metter a user deposits or withdraws shares from a pool:
+        //   1. The pool share will be updated.
+        //   2. User's `amount` gets updated.
+    }
+
+    // Info of each pool.
+    struct PoolInfo {
+        address poolAddress; // Address of pool contract.
+        uint256 allocTokens; // How many tokens allocated in this pool
+        uint256 allocFunds; // How many funds allocated in this pool
+        uint256 OCCPerShare; // Accumulated OCC per share
+    }
+
+  constructor( address _tokenAddress,
+                uint _tokenPrice) public {
     
-    raisingPeriod = _raisingPeriod;
-    icoPeriod = _icoPeriod;
-    distributionPeriod = _distributionPeriod;
+    //stakeholderShare[_ADMIN] 
+    //stakeholderShare[POOL_OPERATOR] 
 
-    minimalFundSize = _minimalFundSize;
-    maximalFundSize = _maximalFundSize;
-
-    minimalDeposit = _minimalDeposit;
-
-    stakeholderShare[RL_ADMIN] = _adminShare;
-    stakeholderShare[RL_POOL_MANAGER] = _poolManagerShare;
-    stakeholderShare[RL_ICO_MANAGER] = DECIMAL_MULTIPLIER - _adminShare - _poolManagerShare;
-
-    setRole_(RL_ADMIN, msg.sender);
-    setRole_(RL_POOL_MANAGER, _poolManagerAddress);
-    setRole_(RL_ICO_MANAGER, _ICOManagerAddress);
-    setRole_(RL_PAYBOT, _paybotAddress);
-
+    setRole_(RR_ADMIN, msg.sender);
+    setRole_(RR_POOL_OPERATOR, _poolOperatorAddress);
+   
     tokenAddress = _tokenAddress;
     tokenPrice = _tokenPrice;
+    }
   }
-}
