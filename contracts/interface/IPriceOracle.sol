897b83b5bf00ec247d34af0d541e6c3536e302c5
Yuriy Chudinov <74604312+chudinovuvaws@users.noreply.github.com>
2020-12-02 00:45:25 +0200

    updated price oracle interface

---
+++ contracts/interface/IPriceOracle.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.4.23;
+
+/************
+@title IPriceOracle interface
+@notice Interface for the Crud price oracle.*/
+interface IPriceOracle {
+    /***********
+    @dev returns the asset price in ETH
+     */
+    function getAssetPrice(address _asset) external view returns (uint256);
+
+    /***********
+    sets the asset price, in wei
+     */
+    function setAssetPrice(address _asset, uint256 _price) external;
+}
